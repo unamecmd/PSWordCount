c6cc209e0fa5e77eee2df347c25b252110d02a31
unamecmd <pnkj4study@gmail.com>
2022-10-14 20:52:01 -0400

    Updated wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -1,9 +1,12 @@
 from pyspark.sql import SparkSession
 from operator import add
 import re
+import datetime
 
 appName = "PySparkWordCount"
 master = "local"
+now = datetime.datetime.now()
+output_file_name = "output_"+(now.strftime("%Y_%m_%d_%H_%M_%S"))
 
 # Create Spark Session
 spark = SparkSession.builder \
@@ -11,11 +14,14 @@ spark = SparkSession.builder \
     .master(master) \
     .getOrCreate()
 
-data_file_path = 'C:/Users/Bhanderi/PycharmProjects/PSWordCount/samplefile.txt'
+bucket_dir= 'gs://pnkj_t1_bucket/'
+output_dir = bucket_dir+'output/'
+sampledata_dir = bucket_dir+'sampledata/'
+outputfile_dir = output_dir+output_file_name
+data_file_path = sampledata_dir+'samplefile.txt'
 
 lines = spark.read.text(data_file_path).rdd.map(lambda x: x[0])
 
-#lines = spark.read.text("C:/Users/Bhanderi/PycharmProjects/PSWordCount/samplefile.txt").rdd.map(lambda x: x[0])
 counts = lines.flatMap(lambda x: x.split(' ')) \
                   .filter(lambda x: re.sub('[^a-zA-Z]+', '', x)) \
                   .filter(lambda x: len(x)>1 ) \
@@ -23,6 +29,8 @@ counts = lines.flatMap(lambda x: x.split(' ')) \
                   .map(lambda x: (x, 1)) \
                   .reduceByKey(add) \
                   .sortByKey()
+
+counts.saveAsTextFile(outputfile_dir)
 
 output = counts.collect()
 for (word, count) in output:
